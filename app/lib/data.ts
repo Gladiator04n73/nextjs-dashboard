0ced6db129e8fb0b6947bf861a4acab7b4d704b1
Xam <gladiator04n73@gmail.com>
2024-10-05 13:43:05 +0300

    linux install

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,6 +41,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -157,7 +158,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
